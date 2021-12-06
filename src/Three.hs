6f8849603015b35650e284788434a1001eed8951
Ed Allonby <edward.allonby@gmail.com>
2021-12-06 21:27:44 +0000

    Add day 3 part 2

--- src/Three.hs
+++ src/Three.hs
@@ -1,39 +1,52 @@
 module Three where
 
-import Control.Applicative
-import Data.Char (digitToInt)
+import Data.Char (digitToInt, intToDigit)
 import Data.List (foldl', tails, transpose)
 import FileUtils (getLines, getNumbers)
 
 solveFirst :: IO ()
 solveFirst = do
   lines <- getPuzzle
-  let rotated = rotr lines
+  let rotated = transpose lines
   let res = fmap count rotated
-  print rotated
-  let gamma = toDec $ fmap mostSignifiant res
-
-  let epsilon = toDec $ fmap leastSignifiant res
+  let gamma = toDec $ fmap mostSignificant res
+  let epsilon = toDec $ fmap leastSignificant res
   print $ gamma * epsilon
 
 solveSecond :: IO ()
 solveSecond = do
-  return ()
+  lines <- getPuzzle
+  let oxygen = head $ untilWithCount ((== 1) . length) (`filterForPosition` mostSignificant) lines
+  let scrubber = head $ untilWithCount ((== 1) . length) (`filterForPosition` leastSignificant) lines
+  print $ binaryStringToInt oxygen * binaryStringToInt scrubber
 
 getPuzzle :: IO [String]
 getPuzzle = getLines "resources/3.txt"
 
-rotr :: [[x]] -> [[x]]
-rotr = map reverse . transpose
+untilWithCount :: ([String] -> Bool) -> (Int -> [String] -> [String]) -> [String] -> [String]
+untilWithCount p f x = run 0 p f x
+  where
+    run n p f x
+      | p x = x
+      | otherwise = run (n + 1) p f (f n x)
+
+filterForPosition :: Int -> ((Int, Int) -> Int) -> [String] -> [String]
+filterForPosition n f lines = filter (\l -> digitToInt (l !! n) == mostCommon) lines
+  where
+    line = transpose lines !! n
+    mostCommon = f $ count line
 
 count :: String -> (Int, Int)
 count = foldl (\(ones, zeros) a -> if digitToInt a == 0 then (ones, zeros + 1) else (ones + 1, zeros)) (0, 0)
 
-mostSignifiant :: (Int, Int) -> Int
-mostSignifiant (ones, zeroes) = if ones >= zeroes then 1 else 0
+mostSignificant :: (Int, Int) -> Int
+mostSignificant (ones, zeroes) = if ones >= zeroes then 1 else 0
 
-leastSignifiant :: (Int, Int) -> Int
-leastSignifiant (ones, zeroes) = if ones <= zeroes then 1 else 0
+leastSignificant :: (Int, Int) -> Int
+leastSignificant (ones, zeroes) = if ones < zeroes then 1 else 0
 
 toDec :: [Int] -> Int
 toDec = foldl' (\acc x -> acc * 2 + x) 0
+
+binaryStringToInt :: String -> Int
+binaryStringToInt = toDec . fmap digitToInt
